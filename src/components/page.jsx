96f33cbf6c36d4c42951d988c44ef75429380b98
PayasSingh <payas@ualberta.ca>
2021-06-09 18:53:27 -0600

    fix broken image links, add some more text

--- src/components/page.jsx
+++ src/components/page.jsx
@@ -10,30 +10,32 @@ return (
     <div className="page">
         <div className="title">
             {console.log("process.env.PUBLIC_URL : ", process.env.PUBLIC_URL)}
-            <img src={process.env.PUBLIC_URL + './img/name.svg'} alt="Name" width="500"/>
+            <img src={process.env.PUBLIC_URL + '/img/name.svg'} alt="Name" width="500"/>
         </div>
         <div className="content">
             <div className="pageLeft">
                 As a programmer, over the years, I have come across multiple languages and frameworks,
-                including, but not limited to, Python, C, Java, JavaScript, MIPS, ReactJS and Django. However,
-                I am the most comfortable in Python and JavaScript. Through personal and course projects
-                I have gained an interest in web development and would love to further this interest in a
-                professional environment!
+                including, but not limited to, <p styles={"color: blue"}>Python</p>, C, Java, JavaScript, MIPS, ReactJS and Django.
+                A lot of my experience lies in Python and JavaScript and I am the most comfortable in
+                these two languages. Through personal and course projects I have gained an interest in
+                web development and would love to further this interest in a professional environment!
                 <br></br>
                 Additionally, I am also very interested in exploring the field of Artificial Intelligence, specially
                 Machine Learning and Reinforcement Learning.
             </div>
             <div className="pageMiddle">
-                <img src={process.env.PUBLIC_URL + './img/brain.svg'} alt="Brain" width="275" height="auto"/>
+                <img src={process.env.PUBLIC_URL + '/img/brain.svg'} alt="Brain" width="275" height="auto"/>
             </div>
             <div className="pageRight">
-                Besides Computer Science, I am also very passionate about Psychology. The field of Cognitive Psychology
-                interests me in particular. I love studying about abstract concepts such as thinking, decision making, percpetion
-                and consciousness. The psychology of language is another field which I am enthusiastic about.
+                Besides Computer Science, I am also very passionate about Psychology.
+                I am a big fan of Cognitive Psychology and love studying about abstract
+                concepts such as thinking, decision making, percpetion and consciousness.
+                I also enjoy reading up on how languages are processed in the brain and I would like
+                expand this interest into CS and dabble in Natural Language Processing.
                 <br></br>
-                I believe that the fields of Computer Science, specifically Artificial Intelligence,
-                and Psychology have a significant overlap. I am currently trying to further my understanding
-                of the interaction between these two fields.
+                I believe that the fields of CS, specifically Artificial Intelligence,
+                and Psychology overlap significantly. Therefore, I am currently trying to further my
+                 understanding of the interaction between these two fields.
             </div>
         </div>
         <div className="footer">
